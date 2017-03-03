dff21fec3d6a34c836de43907b573e174703e10d
eagle <eagle@lotuseed.com>
2017-03-03 13:52:46 +0800

    腾讯 Tars MacOSX 移植版

---
+++ cpp/util/include/util/tc_fcontext_i386.h
@@ -0,0 +1,41 @@
+#ifndef _TC_CONTEXT_FCONTEXT_I386H
+#define _TC_CONTEXT_FCONTEXT_I386H
+
+#include <stdint.h>
+#include <stddef.h>
+
+namespace tars
+{
+
+extern "C" 
+{
+
+struct stack_t
+{
+    void            *   sp;
+    size_t         size;
+
+    stack_t()
+    : sp( 0)
+    , size( 0)
+    {}
+};
+
+struct fcontext_t
+{
+    uint32_t     fc_greg[6];
+    stack_t      fc_stack;
+    uint32_t     fc_freg[2];
+
+    fcontext_t()
+    : fc_greg()
+    , fc_stack()
+    , fc_freg()
+    {}
+};
+
+}
+
+}
+
+#endif
